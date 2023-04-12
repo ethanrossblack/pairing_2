8244ff23007b155fced27457972e55799d882dbc
Ethan Black <ethanrossblack@gmail.com>
2023-04-12 16:39:12 -0600

    Change AND to OR statement

--- plants.rb
+++ plants.rb
@@ -1,8 +1,8 @@
-gets_sunshine = true
+gets_sunshine = false
 oz_of_water = 6
 
-if oz_of_water >= 10 && gets_sunshine == true
-    puts "The plant will THRIVE"
+if oz_of_water >= 10 || gets_sunshine == true
+    puts "The plant will SURVIVE"
 else
     puts "The plant will DIE"
 end
